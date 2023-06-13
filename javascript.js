a603995585a1d8c94bd50e2733a83a4d2a989736
Jack Hastings <32724063+JDHastings@users.noreply.github.com>
2023-06-12 20:43:03 -0400

    Button functionality
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
    
    Added button functionality to add books and remove them and change their
    read status. Still need to add the form to make the input look nice
    
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 let myLibrary = [];
+let removeButtons = document.querySelectorAll('.remove');
+let readButtons = document.querySelectorAll('.read');
+let unreadButtons = document.querySelectorAll('.unread');
 
 function Book(title, author, pages, read){
     this.title = title
@@ -9,30 +12,93 @@ function Book(title, author, pages, read){
 
 function addBookToLibrary(){
     myLibrary.push(new Book(prompt("Title of book: "), prompt("Author of book:"), prompt("Number of pages:"), prompt("Have you read the book?")));
-    
-    const newBook = document.createElement('div');
-    newBook.classList.add('book');
-    const bookTitle = document.createElement('div');
-    bookTitle.classList.add('title');
-    bookTitle.textContent = myLibrary[0].title;
-    const bookAuthor = document.createElement('div');
-    bookAuthor.classList.add('author');
-    bookAuthor.textContent = myLibrary[0].author;
-    const bookPages = document.createElement('div');
-    bookPages.classList.add('pages');
-    bookPages.textContent = myLibrary[0].pages;
-    const bookRead = document.createElement('div');
-    bookRead.classList.add('read');
-    bookRead.textContent = myLibrary[0].read;
-    const bookRemove = document.createElement('button');
-    bookRemove.textContent = "Remove";
+    displayUpdate();
+}
 
-    newBook.append(bookTitle);
-    newBook.append(bookAuthor);
-    newBook.append(bookPages);
-    newBook.append(bookRead);
-    newBook.append(bookRemove);
-    bookContainer.append(newBook);
+function removeBook(){
+    for(let i = 0; i < myLibrary.length; i++){
+        if(this.classList.contains(i)){
+            if(!i){
+                myLibrary.splice(i, i + 1);
+            }else{
+                myLibrary.splice(i, i);
+            }
+            displayUpdate();
+        }
+    }
+}
+
+function changeRead(){
+    if(this.classList.contains('read')){
+        this.classList.remove('read');
+        this.classList.add('unread');
+        this.textContent = "Unread";
+        for(let i = 0; i < myLibrary.length; i++){
+            if(this.classList.contains(i)){
+                myLibrary[i].read = 0;
+            }
+        }
+    }else{
+        this.classList.remove('unread');
+        this.classList.add('read');
+        this.textContent = "Read";
+        for(let i = 0; i < myLibrary.length; i++){
+            if(this.classList.contains(i)){
+                myLibrary[i].read = 1;
+            }
+        }
+    }
+}
+
+function displayUpdate() {
+    const displayedBooks = document.querySelectorAll('.book');
+    displayedBooks.forEach(book => book.remove());
+
+    for(let i = 0; i < myLibrary.length; i++){
+        const newBook = document.createElement('div');
+        newBook.classList.add('book');
+        const bookTitle = document.createElement('div');
+        bookTitle.classList.add('title');
+        bookTitle.textContent = "\"" + myLibrary[i].title + "\"";
+        const bookAuthor = document.createElement('div');
+        bookAuthor.classList.add('author');
+        bookAuthor.textContent = myLibrary[i].author;
+        const bookPages = document.createElement('div');
+        bookPages.classList.add('pages');
+        bookPages.textContent = myLibrary[i].pages + " pages";
+
+        const bookRead = document.createElement('button');
+        if(myLibrary[i].read == 1){
+            bookRead.classList.add('read');
+            bookRead.classList.add(i);
+            bookRead.textContent = "Read";
+        }else{
+            bookRead.classList.add('unread');
+            bookRead.classList.add(i);
+            bookRead.textContent = "Unread";
+        }
+        
+        const bookRemove = document.createElement('button');
+        bookRemove.classList.add('remove');
+        bookRemove.classList.add(i);
+        bookRemove.textContent = "Remove";
+    
+        newBook.append(bookTitle);
+        newBook.append(bookAuthor);
+        newBook.append(bookPages);
+        newBook.append(bookRead);
+        newBook.append(bookRemove);
+        bookContainer.append(newBook);
+        
+        removeButtons = document.querySelectorAll('.remove');
+        removeButtons.forEach(button => button.addEventListener('click', removeBook));
+
+        readButtons = document.querySelectorAll('.read');
+        readButtons.forEach(button => button.addEventListener('click', changeRead));
+
+        unreadButtons = document.querySelectorAll('.unread');
+        unreadButtons.forEach(button => button.addEventListener('click', changeRead));
+    }
 }
 
 const bookContainer = document.querySelector(".main-content");
